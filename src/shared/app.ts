77fbde91e57d034ab3c9630b53974c07e2418b51
matheusfinoto <matheus.finoto@gmail.com>
2022-09-08 13:41:43 -0300

    fix app

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -1,10 +1,30 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { AppError } from '../helper/errorHandler';
 import { router } from '../routes';
+import './container';
 
-const App = express();
+const app = express();
 
-App.use(express.json());
+app.use(express.json());
 
-App.use(router);
+app.use(router);
 
-export { App };
+app.use(
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof AppError) {
+			return response.status(err.statusCode).json({
+				statusCode: err.statusCode,
+				error: err.result,
+				message: err.message,
+			});
+		}
+		return response.status(500).json({
+			statusCode: 500,
+			error: err.message,
+			message: `Internal server error - ${err.message}`,
+		});
+	}
+);
+
+export default app;
